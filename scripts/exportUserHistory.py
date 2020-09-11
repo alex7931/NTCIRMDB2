7efd83243233aaaeccdf9892c597a986537210f7
SijieTao <sijietao@hotmail.com>
2020-09-11 19:27:11 +0900

    Update exportUserHistory.py

--- scripts/exportUserHistory.py
+++ scripts/exportUserHistory.py
@@ -1,22 +1,115 @@
 import pymongo
 import pickle
+from datetime import datetime
+import pandas as pd
 
+# create a dict, users = {userid: [topicid]}
+users1 = pd.read_csv('../data/users1.csv')
+users2 = pd.read_csv('../data/users2.csv')
+users3 = pd.read_csv('../data/users3.csv')
+user_assignments = [users1, users2, users3]
 
-s = {
-'user28':['4'+str(i).rjust(4,'0') for i in [1,11,21,31,41,51,61,71,81,91] ],
-'user29':['4'+str(i).rjust(4,'0') for i in [3,13,23,33,43,53,63,73,83,93] ],
-'user30':['4'+str(i).rjust(4,'0') for i in [5,15,25,35,45,55,65,75,85,95] ],
-'user31':['4'+str(i).rjust(4,'0') for i in [7,17,27,37,47,57,67,77,87,97] ],
-'user32':['4'+str(i).rjust(4,'0') for i in [9,19,29,39,49,59,69,79,89,99] ],
-}
+users = {}
+for df in user_assignments:
+    topic_id = []
+    for id_num in df["topicid"].tolist():
+        d = 4 - len(str(id_num))
+        id_str = '0' * d + str(id_num)
+        topic_id.append(id_str)
+    for user_id in list(df.columns[1:]):
+        temp_topics = [ids for ids in topic_id]
+        sort_or_random = df[user_id].tolist()
+        for idx, flag in enumerate(sort_or_random):
+            if flag:
+                temp_topics[idx] = int('1' + topic_id[idx])
+            else:
+                temp_topics[idx] = int(topic_id[idx])
+        users[user_id] = temp_topics
 
+# connect to database
 con = pymongo.MongoClient("mongodb://localhost:27017/")
 db = con.ntcir
 usrlog = db.usrlog
-dct = {}
-for i in s.keys():
-    temp = {}
-    for j in s[i]:
-        temp[j]=[item for item in usrlog.find({"usrid":i,"topicid":j }).sort([("docid",1),("_id",pymongo.ASCENDING)])]
-    dct[i] = temp
-pickle.dump(dct,open("history_5.pkl","wb"))
+
+user_history = {}
+for userid, topics in users.items():
+    user_logs = {}
+    for topicid in topics:
+        user_logs[topicid]=[item for item in usrlog.find({"usrid": userid, "topicid": str(topicid)}).sort([("docid", 1),("_id" ,pymongo.ASCENDING)])]
+    user_history[userid] = user_logs
+
+
+#Time to judge the first document;
+TJ1D_r = []
+TJ1D_p = []
+#Time to find the first Relevant or Highly relevant document;
+TF1RH_r = []
+TF1RH_p = []
+#Time to find the first Highly relevant document;
+TF1H_r = []
+TF1H_p = []
+#Average time between judging two documents;
+ATBJ_r = []
+ATBJ_p = []
+# Number of times the label of a judged document is corrected to another label.
+NREJ_r = []
+NREJ_p = []
+res=["topicid,userid,TJ1D,TF1RH,TF1H,ATBJ,NREJ\n"]
+for userid in user_history.keys():
+    temp = user_history[userid]
+    for topicid in temp.keys():
+        labeled_doc = set()
+        history = temp[topicid]
+        previousTime = None
+        TJ1D = None
+        TF1RH = None
+        TF1H = None
+        NREJ = 0
+        time_list =[]
+        post_pool={}
+        for idx in range(len(history)):
+            record = history[idx]
+            if record["val"] == "post":
+                post_pool[record["docid"]]= datetime.strptime(record["time"],"%Y-%m-%d %H:%M:%S")
+            else:
+                if record["docid"] not in labeled_doc:
+                    labeled_doc.add(record["docid"])
+                else:
+                    NREJ += 1
+                if post_pool.__contains__(record["docid"]):
+                    judge_time=int((datetime.strptime(record["time"],"%Y-%m-%d %H:%M:%S")-post_pool[record["docid"]]).total_seconds())
+
+                elif idx+1 < len(history) and history[idx+1]["docid"] == record["docid"]:
+                    judge_time=int((datetime.strptime(history[idx+1]["time"],"%Y-%m-%d %H:%M:%S")-datetime.strptime(record["time"],"%Y-%m-%d %H:%M:%S")).total_seconds())
+                else:
+                    judge_time=0
+                if judge_time<=60*3:
+                    time_list.append(judge_time)
+                if not TJ1D:
+                    TJ1D = judge_time
+                if not TF1RH and (record["val"]=="REL" or record["val"]=="H.REL"):
+                    TF1RH = judge_time
+                if not TF1H and record["val"]=="H.REL":
+                    TF1H = judge_time
+        ATBJ = sum(time_list)*1.0/len(time_list)
+        if len(str(topicid))==5:
+            poolType = "sort"
+            tid=str(topicid)[1:]
+            TJ1D_r.append(TJ1D)
+            TF1RH_r.append(TF1RH)
+            TF1H_r.append(TF1H)
+            NREJ_r.append(NREJ)
+            ATBJ_r.append(ATBJ)
+        else:
+            poolType = "rand"
+            tid = str(topicid).rjust(4,"0")
+            TJ1D_p.append(TJ1D)
+            TF1RH_p.append(TF1RH)
+            TF1H_p.append(TF1H)
+            NREJ_p.append(NREJ)
+            ATBJ_p.append(ATBJ)
+
+        res.append(",".join([tid,userid,poolType,str(TJ1D),str(TF1RH),str(TF1H),"%.2f" %ATBJ,str(NREJ)])+"\n")
+out=open("user_history_results.txt","w",encoding="utf8")
+out.writelines(res)
+out.close()
